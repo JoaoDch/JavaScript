625e58f59aef479338e2150ec7e525cb2fdfe8dd
João Victor <116006291+JoaoDch@users.noreply.github.com>
2024-03-26 21:34:09 -0300

    Update script.js

--- 4_Funções/js/script.js
+++ 4_Funções/js/script.js
@@ -169,6 +169,49 @@ function someFunction () {
 
 someFunction()
 
+// 10 - Mais sobre Closure
+// o código define uma função de fechamento para realizar multiplicação e demonstra como criar diferentes funções de multiplicação baseadas nessa função de fechamento.
+const multiplicationClosure = (n) => {
+  return (m) => {
+    return n * m;
+  };
+};
 
+const c1 = multiplicationClosure(5);
 
+const c2 = multiplicationClosure (10);
 
+console.log (c1);
+console.log(c2);
+
+console.log(c1(5));
+console.log(c2(10));
+
+// 11 - Recursion
+
+const untilTen = (n, m) => {
+  if (n < 10) {
+    console.log ("A função parou de executar!")
+  } else {
+    const x = n - m;
+    console.log (x);
+
+    untilTen (x, m);
+  }
+};
+
+untilTen(100, 7);
+
+function factorial (x) {
+  if (x === 0) {
+    return 1
+  } else {
+    return x * factorial (x -1);
+  }
+}
+
+const num = 6;
+
+const result = factorial(num)
+
+console.log (`O fatorial do número ${num} é ${result}`);
